ae5db8a83fc8c3df5c54f6f53784b8703e97a9a2
carlostoek <carlostoek@gmail.com>
2025-06-30 14:59:29 -0600

    Update to Aiogram 3

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -1,11 +1,12 @@
-from aiogram import types, Router
+from aiogram import Router, types
+from aiogram.filters import CommandStart
 from middlewares import UserContext
 from utils.keyboards import MAIN_MENU
 from utils.messages import WELCOME_MESSAGE
 
 router = Router()
 
-@router.message(commands=['start'])
+@router.message(CommandStart())
 async def start(message: types.Message, user_context: UserContext):
     text = WELCOME_MESSAGE.format(
         besitos=user_context.besitos,
